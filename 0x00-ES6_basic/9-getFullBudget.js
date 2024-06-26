d997aee786843e0a39410bbcda6c8264e26b93ba
Abubakr Nasir <abubakr.ezalden.nasir@gmail.com>
2024-06-26 16:54:38 +0000

    fixed eslit errors

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,5 +1,6 @@
+/* eslint-disable import/extensions */
 import getBudgetObject from './7-getBudgetObject.js';
-
+/* eslint-enable import/extensions */
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
